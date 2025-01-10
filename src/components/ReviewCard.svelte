345526ceea111b10fc868bd53acc5f9728dac8b2
Steven Williams <steve@lite.studio>
2025-01-09 21:23:00 -0500

    Fix accessibility: Move figcaption as direct child of figure

--- src/components/ReviewCard.svelte
+++ src/components/ReviewCard.svelte
@@ -14,11 +14,10 @@
   <div class="flex flex-row items-center gap-2">
     <img class="rounded-full" width="32" height="32" alt="" src={img} />
     <div class="flex flex-col">
-      <figcaption class="text-sm font-medium text-[#070513]">
-        {name}
-      </figcaption>
+      <span class="text-sm font-medium text-[#070513]">{name}</span>
       <p class="text-xs font-medium text-[#070513]/60">{username}</p>
     </div>
   </div>
   <blockquote class="mt-2 text-sm text-[#070513]">{body}</blockquote>
+  <figcaption class="sr-only">{name}'s review</figcaption>
 </figure>
